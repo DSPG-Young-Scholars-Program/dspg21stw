32c939a4d3f6ba0f0b35c85737437b6ca33ab5f5
Madeline Garrett <madeline.garrett@colorado.edu>
2021-06-25 15:59:14 -0400

    Running into errors

--- src/get_bgt2.R
+++ src/get_bgt2.R
@@ -15,18 +15,16 @@ conn <- RPostgreSQL::dbConnect(drv = RPostgreSQL::PostgreSQL(),
                                user = "unq6jg",
                                password = "unq6jg")
 
+# Reading in STW SOC Codes 
 setwd("/project/biocomplexity/sdad/projects_data/ncses/stw/original/wioa_2021/stw_2021")
 stw <- read_xlsx("STW_2021.xlsx")
+
+# Classifying as integer 
 stw$SOC <- as.integer(stw$SOC)
 
-View(stw)
 
-tbl <- RPostgreSQL::dbGetQuery(conn = conn, 
-                               statement = "SELECT *
-                               FROM bgt_job.main
-                               WHERE jobdate >= '2019-01-01' AND jobid is not null")
-
-tbl1 <- RPostgreSQL::dbGetQuery(conn = conn,
+# Reading in BGT Data 
+bgt <- RPostgreSQL::dbGetQuery(conn = conn,
                                 statement = "SELECT
                                 onet,
                                 soc,
@@ -39,24 +37,25 @@ tbl1 <- RPostgreSQL::dbGetQuery(conn = conn,
                                 jobid
                                 FROM bgt_job.main
                                 WHERE jobdate >= '2019-01-01' AND jobid is not null
-                                LIMIT 1000")
+                               ")
                               
+bgt_cert <- RPostgreSQL::dbGetQuery(conn = conn, 
+                               statement = "SELECT * 
+                               FROM bgt_job.cert 
+                               LIMIT 30")
 
-View(tbl1)
-summary(tbl)
-summary(tbl$minsalary)
-
-tbl1$soc <- as.integer(tbl1$soc)
+# Transforming variable soc into integer 
+bgt$soc <- as.integer(bgt$soc)
 
 
+# Joining the data source bgt and stw 
+merged <- bgt %>% left_join(stw, by = c("soc" = "SOC"))
 
-d <- tbl1 %>% left_join(stw, by = c("soc" = "SOC"))
 
-View(d)
 
-d2 <- d[is.na(d$NAME) == FALSE,]
+merged_no_na <- merged[is.na(merged$NAME) == FALSE,]
 
-View(d2)
+
 
 # DB Disconnect
 RPostgreSQL::dbDisconnect(conn)
