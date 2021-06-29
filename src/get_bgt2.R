2f425be419adee28965cdbfa52ca9887f173bdac
Madeline Garrett <madeline.garrett@colorado.edu>
2021-06-29 16:32:42 -0400

    BGT STW Data

--- src/get_bgt2.R
+++ src/get_bgt2.R
@@ -12,15 +12,17 @@ conn <- RPostgreSQL::dbConnect(drv = RPostgreSQL::PostgreSQL(),
                                dbname = "sdad",
                                host =  "postgis1",
                                port = 5432,
-                               user = "unq6jg",
-                               password = "unq6jg")
+                               user = Sys.getenv("DB_USR"),
+                               password = Sys.getenv("DB_PWD"))
 
 # Reading in STW SOC Codes 
 setwd("/project/biocomplexity/sdad/projects_data/ncses/stw/original/wioa_2021/stw_2021")
 stw <- read_xlsx("STW_2021.xlsx")
-
+stw$SOC <- gsub("-", "", stw$SOC)
 # Classifying as integer 
 stw$SOC <- as.integer(stw$SOC)
+
+View(stw)
 
 
 # Reading in BGT Data 
@@ -36,16 +38,18 @@ bgt <- RPostgreSQL::dbGetQuery(conn = conn,
                                 fipscounty,
                                 jobid
                                 FROM bgt_job.main
-                                WHERE jobdate >= '2019-01-01' AND jobid is not null
+                                WHERE jobdate >= '2019-01-01' AND jobdate <= '2019-12-31' AND jobid is not null AND soc is not null
                                ")
+
                               
-bgt_cert <- RPostgreSQL::dbGetQuery(conn = conn, 
-                               statement = "SELECT * 
-                               FROM bgt_job.cert 
-                               LIMIT 30")
+bgt_cert <- RPostgreSQL::dbGetQuery(conn = conn,
+                                    statement = "SELECT id, jobdate, certification FROM bgt_job.cert WHERE jobdate >= '2019-01-01' AND jobdate <= '2019-12-31'")
+
 
 # Transforming variable soc into integer 
+bgt$soc <- gsub("-", "", bgt$soc)
 bgt$soc <- as.integer(bgt$soc)
+
 
 
 # Joining the data source bgt and stw 
@@ -55,7 +59,7 @@ merged <- bgt %>% left_join(stw, by = c("soc" = "SOC"))
 
 merged_no_na <- merged[is.na(merged$NAME) == FALSE,]
 
-
-
+#View(merged_no_na)
+#View(stw)
 # DB Disconnect
 RPostgreSQL::dbDisconnect(conn)
