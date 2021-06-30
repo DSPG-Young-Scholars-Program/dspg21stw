c36ea87a4614b01ded7cdbe0b3d5f14141977e7c
Madeline Garrett <madeline.garrett@colorado.edu>
2021-06-30 14:02:47 -0400

    Finished BGT STW Data

--- src/get_bgt2.R
+++ src/get_bgt2.R
@@ -18,11 +18,11 @@ conn <- RPostgreSQL::dbConnect(drv = RPostgreSQL::PostgreSQL(),
 # Reading in STW SOC Codes 
 setwd("/project/biocomplexity/sdad/projects_data/ncses/stw/original/wioa_2021/stw_2021")
 stw <- read_xlsx("STW_2021.xlsx")
-stw$SOC <- gsub("-", "", stw$SOC)
-# Classifying as integer 
-stw$SOC <- as.integer(stw$SOC)
 
-View(stw)
+# Transforming the STW data 
+stw$SOC <- gsub("-", "", stw$SOC) # removing - in soc code 
+stw$SOC <- as.integer(stw$SOC) # making soc code integer 
+# View(stw)
 
 
 # Reading in BGT Data 
@@ -34,32 +34,35 @@ bgt <- RPostgreSQL::dbGetQuery(conn = conn,
                                 sector,
                                 lat,
                                 lon,
-                                fipsstate,
+                                fipsstate, 
                                 fipscounty,
-                                jobid
+                                id
                                 FROM bgt_job.main
                                 WHERE jobdate >= '2019-01-01' AND jobdate <= '2019-12-31' AND jobid is not null AND soc is not null
                                ")
 
-                              
+# Transforming BGT data 
+bgt$soc <- gsub("-", "", bgt$soc) # removing - in soc code 
+bgt$soc <- as.integer(bgt$soc) # making soc code an integer  
+
+# Joining the data source bgt and stw 
+merged <- bgt %>% left_join(stw, by = c("soc" = "SOC"))
+merged_no_na <- merged[is.na(merged$NAME) == FALSE,]
+
+
+# Reading in certification data                              
 bgt_cert <- RPostgreSQL::dbGetQuery(conn = conn,
                                     statement = "SELECT id, jobdate, certification FROM bgt_job.cert WHERE jobdate >= '2019-01-01' AND jobdate <= '2019-12-31'")
 
 
-# Transforming variable soc into integer 
-bgt$soc <- gsub("-", "", bgt$soc)
-bgt$soc <- as.integer(bgt$soc)
+
+# Joining the data source bgt_main (with no na's and no non-stw jobs) and bgt cert 
+merged_cert <- merged_no_na %>% left_join(bgt_cert, by = c("id" = "id")) 
+bgt_stw_cred <- merged_cert[is.na(merged_cert$certification) == FALSE,] # removing jobs with no certification 
 
 
-
-# Joining the data source bgt and stw 
-merged <- bgt %>% left_join(stw, by = c("soc" = "SOC"))
+ggplot(midwest, aes(x=, y=poptotal))
 
 
-
-merged_no_na <- merged[is.na(merged$NAME) == FALSE,]
-
-#View(merged_no_na)
-#View(stw)
 # DB Disconnect
 RPostgreSQL::dbDisconnect(conn)
